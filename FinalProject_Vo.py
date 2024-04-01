f13a1408b48c05d7bd49305c1e96783853844593
Quynh-An <vo.quy@northeastern.edu>
2024-03-31 21:08:10 -0400

    Error handling regarding datafram, attribute and value errors with indexing

--- FinalProject_Vo.py
+++ FinalProject_Vo.py
@@ -12,7 +12,7 @@ import random
 import pandas as pd
 from flask import Flask, render_template, request
 from matplotlib import pylab as plt
-from datetime import datetime
+from datetime import date
 # ==========================================
 # List of symbols, digits, and alphabet
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
@@ -38,25 +38,6 @@ plt.title('Percent of people who have had passwords cracked.')
 # Display the pie chart
 plt.show()
 
-# ============================================
-# Sample data
-data = {
-    'User Email': [],
-    'Generated Password': [],
-    'Date': [],
-    'What is the name of your childhood best friend?': [],
-    'In which city did your parents meet?': [],
-    'What was your first car brand?': [],
-    'What is a nickname you had at home?': [],
-    'What is the name of your first pet?': [],
-    'What is the maiden name of your grandmother?': [],
-    'What is the first concert you attended?': []
-}
-
-# Create DataFrame
-user_data = pd.DataFrame(data)
-
-print(user_data)
 # ============================================
 
 # Option 1 for Passwords
@@ -84,12 +65,14 @@ def option1(password_min, password_max):
         
         print(password_result)
         
-        try_again = input("Press any key to exit. Press enter to create another password. ")
-        if try_again:
+        try_again = input("Press 'y' to create another password. Press another key to exit.")
+        if try_again.lower() != 'y':
             break
  
         return password_result
-    
+ 
+# ============================================
+
 #Option 2 for Passwords
 def option2(password_min, password_max):
     words_to_choose = []
@@ -159,8 +142,67 @@ def option2(password_min, password_max):
             break
         
         return password_result
+
+# =======================================================
+def security_questions():
+    security_questions_answers = []
+    want_questions = input("Would you like to add security questions? If yes, enter 'y': ")
+    
+    if want_questions.lower() == 'y':
+        questions = {
+            1: "What is the name of your childhood best friend?",
+            2: "In which city did your parents meet?",
+            3: "What was your first car brand?",
+            4: "What is a nickname you had at home?",
+            5: "What is the name of your first pet?",
+            6: "What is the maiden name of your grandmother?",
+            7: "What is the first concert you attended?"
+        }
+    
+        # Shuffle the order of questions
+        shuffled_questions = list(questions.items())
+        random.shuffle(shuffled_questions)
+    
+        for question_number, question_text in shuffled_questions:
+            answer = input(f"{question_text}: ")
+            if answer:
+                security_questions_answers.append((question_number, answer))
+            else:
+                security_questions_answers.append((question_number, ""))
+            
+            addtl_questions = input("Would you like to add another security question? If yes, enter 'y': ")
+            if addtl_questions.lower() != 'y':
+                break
+          
+        # Check if all questions have been answered
+        for question_number, _ in questions.items():
+            if not any(q[0] == question_number for q in security_questions_answers):
+                security_questions_answers.append((question_number, None))
+        
+    security_questions_answers = sorted(security_questions_answers)
+        
+    return security_questions_answers 
+
+# ================================================
     
 def main():
+
+    # dataframe outline
+    data = {
+        'User Email': ['sample@gmail.com'],
+        'Generated Password': ['MyPassword.123'],
+        'Date': ['2024-31-03'],
+        'What is the name of your childhood best friend?': ['Abby Smith'],
+        'In which city did your parents meet?': ['New York'],
+        'What was your first car brand?': ['Nissam Altima'],
+        'What is a nickname you had at home?': ['Blue'],
+        'What is the name of your first pet?': ['Spot'],
+        'What is the maiden name of your grandmother?': ['Johnson'],
+        'What is the first concert you attended?': ['One Direction']
+    }
+
+    # Create DataFrame
+    user_data = pd.DataFrame(data)
     print("""
     This is a password generator. It can give you two types of passwords. You can either have 1) a randomized set of letters,
     symbols, and numbers OR  2) you can have strings put together with symbols and numbers. Please base your choice on the needs of the password.
@@ -170,7 +212,7 @@ def main():
         
         # Ask for user email and get a date
         email = input("First, enter an email: ")
-        date = datetime.now()
+        password_date = date.today()
         
         password_type = input("What type of password are you looking for? Enter '1' for option 1, and '2' for option 2: ")
         if password_type == "1":
@@ -212,91 +254,25 @@ def main():
     security_data = security_questions()
         
     new_data = {
-        'User Email': email ,
-        'Generated Password': final_password,
-        'Date': date,
-        'What is the name of your childhood best friend?': security_data[0][1],
-        'In which city did your parents meet?': security_data[1][1],
-        'What was your first car brand?': security_data[2][1],
-        'What is a nickname you had at home?': security_data[3][1],
-        'What is the name of your first pet?': security_data[4][1],
-        'What is the maiden name of your grandmother?': security_data[5][1],
-        'What is the first concert you attended?': security_data[6][1]
+        'User Email': [email] ,
+        'Generated Password': [final_password],
+        'Date': [password_date],
+        'What is the name of your childhood best friend?': [security_data[0][1]],
+        'In which city did your parents meet?': [security_data[1][1]],
+        'What was your first car brand?': [security_data[2][1]],
+        'What is a nickname you had at home?': [security_data[3][1]],
+        'What is the name of your first pet?': [security_data[4][1]],
+        'What is the maiden name of your grandmother?': [security_data[5][1]],
+        'What is the first concert you attended?': [security_data[6][1]]
         }
             
-    print(new_data)
+    user_data = pd.concat([user_data, pd.DataFrame(new_data)], ignore_index=True)
     
-def security_questions():
-    security_questions_answers = []
-    want_questions = input("Would you like to add security questions? If yes, enter 'y': ")
-    
-    if want_questions.lower() == 'y':
-        questions = {
-            1: "What is the name of your childhood best friend?",
-            2: "In which city did your parents meet?",
-            3: "What was your first car brand?",
-            4: "What is a nickname you had at home?",
-            5: "What is the name of your first pet?",
-            6: "What is the maiden name of your grandmother?",
-            7: "What is the first concert you attended?"
-        }
-    
-        # Shuffle the order of questions
-        shuffled_questions = list(questions.items())
-        random.shuffle(shuffled_questions)
-    
-        for question_number, question_text in shuffled_questions:
-            answer = input(f"{question_text}: ")
-            if answer:
-                security_questions_answers.append((question_number, answer))
-            else:
-                security_questions_answers.append((question_number, ""))
-            
-            addtl_questions = input("Would you like to add another security question? If yes, enter 'y': ")
-            if addtl_questions.lower() != 'y':
-                break
-          
-        # Check if all questions have been answered
-        for question_number, _ in questions.items():
-            if not any(q[0] == question_number for q in security_questions_answers):
-                security_questions_answers.append((question_number, ""))
-        
-    security_questions_answers = sorted(security_questions_answers)
+    print(user_data)
 
-    print(security_questions_answers)
-        
-
-    return security_questions_answers 
      
 main()      
             
 
 
-
-# Option 3 Most Memorable using Personal Questions
-    
-
-# Not sure how to code this yet, but writing my general ideas
-        
-# option 2 idea
-# import the list of words created in TXT file, read into something
-# if there is a way to organize and them randomize a number and select that corresponding number to the words in the txt file that is ideal
-# ranodmize how many numbers and symbols are needed between 1-4
-# keep making strings of words awith numbers and symbols at the end until we fit in the minimum and maximum
-    # example if the first is RedFlamingoLion98$ but this is too many characters, then it can keep making randomized words until
-    # RedDogPillow54#, which is shorter
-    
-# print back out to the user the result and ask if they like it
-# do it again if they do not and keep if they do
-
-# find a way to ask for email and email them when they next need to change the password??? If possible
-# Use pandas to create a database
-# Ask for the user emails when they get their password if they want to be reminded
-# make csv file and save with pandas in a dataframe to calculate the time based on the date of password entry
-# create a time since email and function that checks its limit and then call function to send an email
-# I have to look into services to send emails
-
-# Use flask to have a UI platform
-# Use data visualizations to show maybe statistics on password protection just to use more of what we learned in class
-# I can also use pandas for password verification questions
 
